eaf1fb34397dca03b9b6f2a31ba9e817d85db751
timmy <timmymill87@gmail.com>
2016-09-27 12:50:46 -0500

    Got gameplay working. Player is able to play a simple round of craps.

--- gameplay.py
+++ gameplay.py
@@ -7,12 +7,12 @@ def player_turn(player):
     # Puck Off
     # PassLine Bet
     # Shooter Rolls
-    number_rolled = Player(player.roll_die())
+    number_rolled = player.roll_die()
     # Come Out Roll
-    come_out_roll(number_rolled)
+    come_out_roll(number_rolled, player)
 
 
-def come_out_roll(number_rolled):
+def come_out_roll(number_rolled, player):
     # Roll
     if number_rolled == 7 or number_rolled == 11:
         # player wins
@@ -22,19 +22,21 @@ def come_out_roll(number_rolled):
         print("You lose")
     else:
         # player goes to point round
-        point_round(number_rolled)
+        point_round(number_rolled, player)
 
 
-def point_round(number_rolled):
+def point_round(number_rolled, player):
     # Marker On
     point = number_rolled
+    print("The point is " + str(number_rolled))
     while True:
         # Roll
-        if point == 7:
+        number_rolled = player.roll_die()
+        if number_rolled == 7:
             # player loses, and the next player gets the die
             print("Seven Out")
             return
-        elif point == number_rolled:
+        elif number_rolled == point:
             # player wins
             print("You win")
             return
